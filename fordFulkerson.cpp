91e76f5684e8add4485317d9bc5668d644e12315
rafaemilima <53321503+rafaemilima@users.noreply.github.com>
2021-09-23 11:03:29 -0300

    Upload FF algorithm

---
+++ fordFulkerson.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct FFgraph
+{
+    int V{};
+    int E{};
+    vector<vector<int>> adj_list;
+} typedef FFgraph;
+
+bool bfs(const FFgraph& my_graph, vector<vector<int>> res, int start_v, int destiny_v, int par[]){
+
+    int sfila;
+    bool visited[my_graph.V];
+    memset(visited, false, sizeof(bool) * my_graph.V);
+    visited[start_v] = true;
+    queue<int> queue;
+    queue.push(start_v);
+
+    while(!queue.empty()){
+        sfila = queue.front();
+        queue.pop();
+
+        for(int i = 0; i < my_graph.V; i++){
+            if(res[sfila][i] > 0 && !visited[i]){
+                par[i] = sfila;
+                visited[i] = true;
+                queue.push(i);
+            }
+        }
+    }
+
+    return visited[destiny_v];
+}
+
+
+int fordFulkerson(FFgraph graph, int start_v, int destiny_v){
+    int u, v, maxFlow = 0;
+    int *par = new int[graph.V];
+    vector<vector<int>> res;
+    res.resize(graph.V);
+
+    for(int i = 0; i < graph.V; i++){
+        res[i].resize(graph.V);
+
+        for(int j = 0; j < graph.V; j++){
+            res[i][j] = graph.adj_list[i][j];
+        }
+    }
+
+    while(bfs(graph, res, start_v, destiny_v, par)){
+        int path = 99999;
+
+        for (v = destiny_v; v != start_v; v = par[v]){
+            u = par[v];
+            if(res[u][v] < path){
+                path = res[u][v];
+            }
+        }
+
+        for(v = destiny_v; v != start_v; v = par[v]){
+            u = par[v];
+            res[u][v] -= path;
+            res[v][u] += path;
+        }
+        maxFlow += path;
+    }
+
+    return maxFlow;
+}
+
+int main() {
+    FFgraph myGraph;
+
+    int u, v, weight, start_v, destiny_v;
+
+    cin >> myGraph.V >> myGraph.E;
+
+    myGraph.adj_list.resize(myGraph.V);
+
+    for (int i = 0; i < myGraph.V; i++) {
+        myGraph.adj_list[i].resize(myGraph.V);
+    }
+
+    for (int i = 0; i < myGraph.E; i++) {
+        cin >> u >> v >> weight;
+        myGraph.adj_list[u][v] = weight;
+    }
+    cin >> start_v >> destiny_v;
+    cout << "The maximum flow from vector "<<start_v <<" to vector "<<destiny_v<< " is: " << fordFulkerson(myGraph, start_v, destiny_v) << "\n";
+
+    return 0;
+}
